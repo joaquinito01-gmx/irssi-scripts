366d4e5051509c6bb779630ec4d8cb4f6a3fd775
Joaquinito01 <76450680+joaquinito01-gmx@users.noreply.github.com>
2021-02-28 14:25:19 -0600

    we can use whois now

--- security.pl
+++ security.pl
@@ -12,6 +12,7 @@ $VERSION = '1.00';
         license         => '',
         changed         => ""
 );
+our @whois = ();
 
 sub skb
 {
@@ -23,8 +24,34 @@ sub skb
                 Irssi::print("Security: No kick target was given. Ban cannot be complete.");
                 Irssi::signal_stop();
         }
-        $server->command("BAN $witem->{name} $spl[0]");
-        $server->command("KICK $witem->{name} $spl[0] Your behaviour is not conducive the the desired enviornment.");
+        unshift @whois, "$spl[0],$witem->{name}";
+        $server->command("WHOIS $spl[0]");
+}
+
+sub doban
+{
+        my($server, $chan, $client, $username, $hostname, $realname) = @_;
+        $server->command("BAN $chan *!*\@$hostname");
+        $server->command("KICK $chan $client Your behaviour is not conducive the the desired enviornment.");
+}
+
+sub whoisuser
+{
+        my($server, $data) = @_;
+        my @info = split(' ', $data);
+        my $client = $info[1];my $username = $info[2];my $hostname = $info[3];my $realname = $info[5];
+        my $index = 0;
+        foreach my $whoi (@whois) {
+                $index++;
+                my @inf = split(',', $whoi);
+                my $chan = $inf[1];my $nick = $inf[0];
+                if ($nick == $client) {
+                        doban($server, $chan, $client, $username, $hostname, $realname);
+                        splice(@whois, $index, 1);
+
+                }
+        }
 }
 
 Irssi::command_bind skb => \&skb;
+Irssi::signal_add("event 311", "whoisuser")
